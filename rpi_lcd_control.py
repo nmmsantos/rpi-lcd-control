c264e3fba5f626c9ed63dfd7f91a001ac066a021
Nuno Santos <eu.nuno@gmail.com>
2021-06-27 21:58:23 +0100

    Fix distance 0 bug

--- rpi_lcd_control.py
+++ rpi_lcd_control.py
@@ -1,42 +1,46 @@
 #!/usr/bin/env python
 
 import time
-import VL53L0X
-from os import system
+from subprocess import run  # nosec
 
-tof = VL53L0X.VL53L0X(i2c_bus=1,i2c_address=0x29)
+import VL53L0X
+
+vcgencmd = "/usr/bin/vcgencmd"
+
+tof = VL53L0X.VL53L0X(i2c_bus=1, i2c_address=0x29)
 tof.open()
-tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.BETTER)
+tof.start_ranging(VL53L0X.Vl53l0xAccuracyMode.GOOD)
 
 timing = tof.get_timing()
 
-if timing < 100000:
-    timing = 100000
+if timing < 20000:
+    timing = 20000
 
-sleeptime = timing/1000000.00
+sleeptime = timing / 1000000.00
 
-print("Timing %d ms" % (timing/1000))
+print("Timing %d ms" % (timing / 1000))
 
 ison = True
-system('vcgencmd display_power 1')
+run([vcgencmd, "display_power", "1"], shell=False, check=True)  # nosec
 countdown = 0
 
 try:
     while True:
         distance = tof.get_distance()
 
-        if distance < 6000:
-            countdown = 0
+        if distance > 0:
+            if distance < 6000:
+                countdown = 0
 
-            if not ison:
-                ison = True
-                system('vcgencmd display_power 1')
-        else:
-            if ison:
-                countdown += sleeptime
-                if countdown > 3:
-                    ison = False
-                    system('vcgencmd display_power 0')
+                if not ison:
+                    ison = True
+                    run([vcgencmd, "display_power", "1"], shell=False, check=True)  # nosec
+            else:
+                if ison:
+                    countdown += sleeptime
+                    if countdown > 3:
+                        ison = False
+                        run([vcgencmd, "display_power", "0"], shell=False, check=True)  # nosec
 
         time.sleep(sleeptime)
 except KeyboardInterrupt:
